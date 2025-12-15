0410459116de02370e340b6fbfce372eefd4b4d9
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2025-12-15 16:05:51 +0300

    refactoring, cursor/fish position

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"image/color"
 	"log"
 	"math/rand/v2"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	// "github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
@@ -51,10 +53,11 @@ type Game struct {
 	Record      int
 	Progress    float32
 	IsColliding bool
-	Pressing    bool
+	IsPressing  bool
 	D           float32
 	IsCatching  bool
 	Stage       GameStage
+	EndTime     time.Time
 }
 
 func (g *Game) Collide() bool {
@@ -67,27 +70,33 @@ func (g *Game) Collide() bool {
 	return fishStart >= cursorStart && fishEnd <= cursorEnd
 }
 
-func (g *Game) Update() error {
-	isMousePressed := ebiten.IsMouseButtonPressed(ebiten.MouseButton0)
-	isSpacePressed := ebiten.IsKeyPressed(ebiten.KeySpace)
-	g.Pressing = isSpacePressed || isMousePressed
+func (g *Game) Start() {
+	g.Skill = 0.7
+	g.Score = 0
+	g.Stage = GAME
+}
 
-	if g.Stage == INTRO || g.Stage == SCORE {
-		if g.IsCatching && g.Pressing {
-			return nil
-		}
+func (g *Game) Catch() {
+	g.Progress = 0
+	g.Skill -= g.Skill * 0.05
+	g.Score += 1
 
-		g.IsCatching = false
-
-		if g.Pressing {
-			g.Skill = 0.7
-			g.Score = 0
-			g.Stage = GAME
-		}
-
-		return nil
+	if g.Score > g.Record {
+		g.Record = g.Score
 	}
 
+	g.Fish = 0.1
+	g.Cursor = 0.8
+	g.IsCatching = false
+}
+
+func (g *Game) Finish() {
+	g.Stage = SCORE
+	g.IsCatching = false
+	g.EndTime = time.Now()
+}
+
+func (g *Game) VobbleFish() {
 	if rand.Float32() > 0.9 {
 		g.D *= -1.0
 	}
@@ -101,8 +110,10 @@ func (g *Game) Update() error {
 	if g.Fish < 0 {
 		g.Fish = 0.0
 	}
+}
 
-	if g.Pressing {
+func (g *Game) MoveCursor() {
+	if g.IsPressing {
 		g.Cursor -= 0.01
 	} else {
 		g.Cursor += 0.01
@@ -114,6 +125,35 @@ func (g *Game) Update() error {
 	if g.Cursor < 0 {
 		g.Cursor = 0.0
 	}
+}
+
+func (g *Game) ListenPressing() {
+	isMousePressed := ebiten.IsMouseButtonPressed(ebiten.MouseButton0)
+	isSpacePressed := ebiten.IsKeyPressed(ebiten.KeySpace)
+	// TODO: touches
+	g.IsPressing = isSpacePressed || isMousePressed
+}
+
+func (g *Game) Update() error {
+	g.ListenPressing()
+
+	if g.Stage == INTRO && g.IsPressing {
+		g.Start()
+		return nil
+	}
+
+	if g.Stage == SCORE && g.IsPressing {
+		if time.Since(g.EndTime) < 1*time.Second {
+			return nil
+		}
+
+		g.Start()
+		return nil
+	}
+
+	g.VobbleFish()
+
+	g.MoveCursor()
 
 	g.IsColliding = g.Collide()
 
@@ -122,14 +162,7 @@ func (g *Game) Update() error {
 		g.Progress += 0.01
 
 		if g.Progress >= 1.0 {
-			g.Progress = 0
-			g.Skill -= g.Skill * 0.05
-			g.Score += 1
-			if g.Score > g.Record {
-				g.Record = g.Score
-			}
-			g.Fish = rand.Float32()
-			g.IsCatching = false
+			g.Catch()
 		}
 
 		return nil
@@ -141,7 +174,7 @@ func (g *Game) Update() error {
 		g.Progress = 0.0
 
 		if g.IsCatching {
-			g.Stage = SCORE
+			g.Finish()
 		}
 	}
 
@@ -282,11 +315,10 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	game := Game{
-		Cursor:   0.65,
-		Skill:    0.7,
-		Progress: 0.7,
-		Fish:     0.7,
-		D:        1.0,
+		Cursor: 0.8,
+		Skill:  0.7,
+		Fish:   0.1,
+		D:      1.0,
 	}
 
 	ebiten.SetWindowSize(screenW, screenH)
